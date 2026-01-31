b89e5faf50157163cbc775ad70e2e5a057aed05a
Bananaska <romanzlobin10@mail.ru>
2026-01-31 16:48:25 +0700

    Z

--- Assets/Scripts/SimpleProjectile.cs
+++ Assets/Scripts/SimpleProjectile.cs
@@ -24,7 +24,7 @@ public class SimpleProjectile : MonoBehaviour
 
             //Destroy(gameObject);
         }
-        else if (!other.CompareTag("Player") && !other.CompareTag("Projectile"))
+        else if (!other)
         {
             // ”ничтожаем при столкновении с окружением
             Destroy(gameObject);
