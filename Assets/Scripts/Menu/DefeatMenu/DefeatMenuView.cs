0de4c720c2778cceba62efa07e363f1dce39d118
Bananaska <romanzlobin10@mail.ru>
2026-02-28 16:50:19 +0700

    28-02-26

--- Assets/Scripts/Menu/DefeatMenu/DefeatMenuView.cs
+++ Assets/Scripts/Menu/DefeatMenu/DefeatMenuView.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DefatMenuView : MonoBehaviour
+public class DefeatMenuView : MonoBehaviour
 {
     [SerializeField] private Button _menuButton;
 
